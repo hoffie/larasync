d1c910587147d04b7afcba174631829bd6bcd701
Christian Hoffmann <mail@hoffmann-christian.info>
2015-01-05 16:12:24 +0100

    repository: implementing chunker

---
+++ repository/chunker.go
@@ -0,0 +1,60 @@
+package repository
+
+import (
+	"errors"
+	"io"
+	"os"
+)
+
+// Chunker returns the contents of the given file path in chunkSize-sized
+// chunks.
+type Chunker struct {
+	file      *os.File
+	finished  bool
+	chunkSize uint64
+}
+
+// ErrBadChunkSize will be thrown if a too little chunk size is requested
+var ErrBadChunkSize = errors.New("bad chunk size (must be >16 bytes)")
+
+// NewChunker returns a new chunker instance for the given file path
+// and the given chunk size.
+func NewChunker(path string, chunkSize uint64) (*Chunker, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+
+	if chunkSize < 16 {
+		return nil, BadChunkSize
+	}
+
+	c := &Chunker{
+		file:      file,
+		finished:  false,
+		chunkSize: chunkSize,
+	}
+	return c, nil
+}
+
+// HasNext returns true if an upcoming Next() call is expected
+// to return more data.
+func (c *Chunker) HasNext() bool {
+	return !c.finished
+}
+
+// Next returns the next part of the file, with at most chunkSize bytes.
+func (c *Chunker) Next() ([]byte, error) {
+	buf := make([]byte, c.chunkSize)
+	numBytes, err := c.file.Read(buf)
+	if uint64(numBytes) < c.chunkSize || err != nil {
+		c.finished = true
+	}
+	if err != nil || numBytes == 0 {
+		c.file.Close()
+	}
+	if err != nil && err != io.EOF {
+		return nil, err
+	}
+	return buf[:numBytes], nil
+}
---
+++ repository/chunker_test.go
@@ -0,0 +1,47 @@
+package repository
+
+import (
+	"io/ioutil"
+	"path/filepath"
+
+	. "gopkg.in/check.v1"
+)
+
+type ChunkerTests struct {
+	dir string
+}
+
+var _ = Suite(&ChunkerTests{})
+
+func (t *ChunkerTests) SetUpTest(c *C) {
+	t.dir = c.MkDir()
+}
+
+func (t *ChunkerTests) TestHandleError(c *C) {
+	_, err := NewChunker(filepath.Join(t.dir, "non-existing"), 16)
+	c.Assert(err, NotNil)
+}
+
+func (t *ChunkerTests) TestHandleBadChunkSiez(c *C) {
+	_, err := NewChunker("test", 15)
+	c.Assert(err, NotNil)
+}
+
+func (t *ChunkerTests) TestNormal(c *C) {
+	path := filepath.Join(t.dir, "test")
+	err := ioutil.WriteFile(path, []byte("12345678901234567890"),
+		0600)
+	c.Assert(err, IsNil)
+
+	ch, err := NewChunker(path, 16)
+	c.Assert(err, IsNil)
+	c.Assert(ch.HasNext(), Equals, true)
+	chunk, err := ch.Next()
+	c.Assert(err, IsNil)
+	c.Assert(chunk, DeepEquals, []byte("1234567890123456"))
+	c.Assert(ch.HasNext(), Equals, true)
+	chunk, err = ch.Next()
+	c.Assert(err, IsNil)
+	c.Assert(chunk, DeepEquals, []byte("7890"))
+	c.Assert(ch.HasNext(), Equals, false)
+}

642a5cd590e57df3b93513375896d158139243fb
Christian Hoffmann <christian@hoffie.info>
2015-01-13 00:24:55 +0100

    repository.StateConfig: comments

--- repository/stateConfig.go
+++ repository/stateConfig.go
@@ -5,11 +5,14 @@ import (
 	"io/ioutil"
 )
 
+// StateConfig is used to keep track of state information
+// which has be be read and written programatically.
 type StateConfig struct {
 	Path          string `json:"-"`
 	DefaultServer string
 }
 
+// Load attempts to load previous state config from disk.
 func (sc *StateConfig) Load() error {
 	data, err := ioutil.ReadFile(sc.Path)
 	if err != nil {
@@ -19,6 +22,7 @@ func (sc *StateConfig) Load() error {
 	return nil
 }
 
+// Save serializes the current StateConfig to disk.
 func (sc *StateConfig) Save() error {
 	data, err := json.MarshalIndent(sc, "", "  ")
 	if err != nil {

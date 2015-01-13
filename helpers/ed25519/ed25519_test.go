3ae173327f235337df9d38286861d0e3a4e5ded3
Christoph Brand <ch.brand@scolterius.net>
2015-01-13 12:36:49 +0100

    helpers: added key gen to the edhelpers.

--- helpers/ed25519/ed25519_test.go
+++ helpers/ed25519/ed25519_test.go
@@ -2,7 +2,6 @@ package ed25519
 
 import (
 	"bytes"
-	e "github.com/agl/ed25519"
 	"testing"
 
 	. "gopkg.in/check.v1"
@@ -18,9 +17,14 @@ func Test(t *testing.T) {
 
 func (t *Tests) TestEd25519GetPublicFromPrivate(c *C) {
 	fakeRandReader := bytes.NewBufferString("012345678901234567890123456789012")
-	pub, priv, err := e.GenerateKey(fakeRandReader)
+	pub, priv, err := GenerateKeyFrom(fakeRandReader)
 	c.Assert(err, IsNil)
 	myPub := GetPublicKeyFromPrivate(*priv)
 	c.Assert(err, IsNil)
 	c.Assert(*pub, DeepEquals, myPub)
+}
+
+func (t *Tests) TestGenerateKey(c *C) {
+	_, _, err := GenerateKey()
+	c.Assert(err, IsNil)
 }

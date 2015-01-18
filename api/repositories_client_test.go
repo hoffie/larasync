b4cff71d8e6d3bced67a2ef32e5ff4ac9f4c5cea
Christoph Brand <ch.brand@scolterius.net>
2015-01-19 00:56:33 +0100

    api: Added tests for the repositories_client.go file.

---
+++ api/repositories_client_test.go
@@ -0,0 +1,27 @@
+package api
+
+import (
+	. "gopkg.in/check.v1"
+)
+
+type RepositoriesClientTest struct {
+	BaseClientTest
+}
+
+var _ = Suite(&RepositoriesClientTest{newBaseClientTest()})
+
+func (t *RepositoriesClientTest) SetUpTest(c *C) {
+	t.BaseClientTest.SetUpTest(c)
+	t.client.SetAdminSecret(adminSecret)
+}
+
+func (t *RepositoriesClientTest) TestRegister(c *C) {
+	err := t.client.Register(t.pubKey)
+	c.Assert(err, IsNil)
+}
+
+func (t *RepositoriesClientTest) TestConnError(c *C) {
+	t.server.Close()
+	err := t.client.Register(t.pubKey)
+	c.Assert(err, NotNil)
+}

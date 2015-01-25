896407ed80d5b1ed9c638631d776e8db4e5b6a5f
Christoph Brand <ch.brand@scolterius.net>
2015-01-26 00:45:36 +0100

    api: Uploader extended with the ability to upload delta mechanisms.

--- api/client/uploader.go
+++ api/client/uploader.go
@@ -24,7 +24,87 @@ type Uploader struct {
 
 // PushAll ensures that the remote state is synced with the local state.
 func (ul *Uploader) PushAll() error {
-	return ul.uploadNIBs()
+	r := ul.r
+	transaction, err := r.CurrentTransaction()
+	if err != nil {
+		return err
+	}
+	err := ul.PushAll()
+	if err != nil {
+		return err
+	}
+	return ul.saveLastUploadedTransaction(transaction)
+}
+
+// PushDelta pushes all nibs from the stored local transaction id.
+func (ul *Uploader) PushDelta() error {
+	r := ul.r
+	s, err := r.StateConfig()
+	if err != nil {
+		return err
+	}
+
+	defaultServer := s.DefaultServer
+	return ul.pushFromTransactionID(defaultServer.LocalTransactionID)
+}
+
+// PushFromTransactionID pushes all NIBs which have been entered after
+// the given local transaction ID.
+func (ul *Uploader) pushFromTransactionID(transactionID int64) error {
+	r := ul.r
+	transactions, err := r.TransactionsFrom(transactionID)
+	if err != nil {
+		return fmt.Errorf("unable to get transactions (%s)", err)
+	}
+
+	var lastTransaction *repository.Transaction
+	for _, transaction := range transactions {
+		err = ul.uploadTransaction(transaction)
+		if err != nil {
+			return err
+		}
+		lastTransaction = transaction
+	}
+
+	if lastTransaction != nil {
+		err = ul.saveLastUploadedTransaction(lastTransaction)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+// saveLastUploadedTransaction takes the given transaction and configures it to the
+// state config to store it as the last transaction.
+func (ul *Uploader) saveLastUploadedTransaction(transaction *repository.Transaction) error {
+	s, err := r.StateConfig()
+	if err != nil {
+		return nil
+	}
+	s.DefaultServer.LocalTransactionID = lastTransaction.ID
+	err = s.Save()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// uploadTransaction uploads all nibs in the added transaction.
+func (ul *Uploader) uploadTransaction(transaction *repository.Transaction) error {
+	r := ul.r
+	for _, nibID := range transaction.NIBIDs {
+		nib, err := r.GetNIB(nibID)
+		if err != nil {
+			return fmt.Errorf("could not load NIB with id %s (%s)", nibID, err)
+		}
+		err = ul.uploadNIB(nib)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
 }
 
 // uploadNIBs uploads all local NIBs and content of the NIBs to

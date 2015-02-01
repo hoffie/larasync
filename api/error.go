d5096dffc190d8b0c64cd53de7a9c773983f0b41
Christoph Brand <ch.brand@scolterius.net>
2015-02-01 16:58:36 +0100

    api: go vet fixes.

--- api/error.go
+++ api/error.go
@@ -4,14 +4,14 @@ package api
 // to the client when a JSON endpoint encounters
 // an error
 type JSONError struct {
-	Type  string `json:error_type`
+	Type  string `json:"error_type"`
 	Error string `json:"error"`
 }
 
 // ContentIDsJSONError gets returned if there is content
 // being referenced in an uploaded NIB is missing.
 type ContentIDsJSONError struct {
-	Type              string   `json:error_type`
+	Type              string   `json:"error_type"`
 	Error             string   `json:"error"`
 	MissingContentIDs []string `json:"missing_content_ids"`
 }

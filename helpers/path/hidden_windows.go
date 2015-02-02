8c966dea6dd5ee6058b1c9d0baa6390749df009b
Christoph Brand <ch.brand@scolterius.net>
2015-02-02 16:28:26 +0100

    helpers: Added windows specific functionality to hide a file path.

---
+++ helpers/path/hidden_windows.go
@@ -0,0 +1,41 @@
+package path
+
+import (
+    "syscall"
+)
+
+// getFileAttributes returns the file Attributes for the
+// given path.
+func getFileAttributes(path string) (uint32, error) {
+    p, err := syscall.UTF16PtrFromString(path)
+    if err != nil {
+        return 0, err
+    }
+    return syscall.GetFileAttributes(p)
+}
+
+// IsHidden returns if the given path is marked as hidden in windows.
+func IsHidden(path string) (bool, error) {
+    attrs, err := getFileAttributes(path)
+    if err != nil {
+        return false, err
+    }
+    return attrs & syscall.FILE_ATTRIBUTE_HIDDEN != 0, nil
+}
+
+// Hide tries to hide the given directory and returns an error
+// if failed.
+func Hide(path string) (error) {
+    attrs, err := getFileAttributes(path)
+    if err != nil {
+        return err
+    }
+
+    p, err := syscall.UTF16PtrFromString(path)
+    if err != nil {
+        return err
+    }
+
+    attrs = attrs | syscall.FILE_ATTRIBUTE_HIDDEN
+    return syscall.SetFileAttributes(p, attrs)
+}
---
+++ helpers/path/hidden_windows_test.go
@@ -0,0 +1,47 @@
+package path
+
+import (
+    "os"
+    "path/filepath"
+
+    . "gopkg.in/check.v1"
+)
+
+var _ = Suite(&HiddenWindowsTests{})
+
+type HiddenWindowsTests struct {
+    dir    string
+}
+
+func (t *HiddenWindowsTests) SetUpTest(c *C) {
+    t.dir = c.MkDir()
+    c.Assert(os.Mkdir(t.getDir(), 0700), IsNil)
+}
+
+func (t *HiddenWindowsTests) getDir() string {
+    return filepath.Join(t.dir, "test")
+}
+
+func (t *HiddenWindowsTests) TestDirectoryHidden(c *C) {
+    res, err := IsHidden(t.getDir())
+    c.Assert(err, IsNil)
+    c.Assert(res, Equals, false)
+}
+
+func (t *HiddenWindowsTests) TestHideDirectory(c *C) {
+    c.Assert(Hide(t.getDir()), IsNil)
+    res, err := IsHidden(t.getDir())
+    c.Assert(err, IsNil)
+    c.Assert(res, Equals, true)
+}
+
+func (t *HiddenWindowsTests) TestError(c *C) {
+    c.Assert(os.Remove(t.getDir()), IsNil)
+    c.Assert(Hide(t.getDir()), NotNil)
+}
+
+func (t *HiddenWindowsTests) TestCheckError(c *C) {
+    c.Assert(os.Remove(t.getDir()), IsNil)
+    _, err := IsHidden(t.getDir())
+    c.Assert(err, NotNil)
+}
